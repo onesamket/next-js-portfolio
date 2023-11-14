e27ea7beb82807f33358a316ded206d1acfc0335
Tewodros Birhanu <onesamket@gmail.com>
2023-11-14 04:57:11 -0800

    fix responsiveness bug

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,15 @@ const config: Config = {
 
   ],
   darkMode: 'class',
+  theme: {
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+    },
+  },
   plugins: [nextui()],
 }
 export default config
